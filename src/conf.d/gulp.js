d0e347eb0e39b3b3a8065c251cda7cf6b39d2fce
Aaron John Schlosser <aaron@aaronschlosser.com>
2015-05-03 12:14:41 -0700

    add license to gulp conf

--- src/conf.d/gulp.js
+++ src/conf.d/gulp.js
@@ -1,3 +1,30 @@
+// --------------------------------------------------
+// LICENSE
+
+/*
+	This file is part of Tile.
+
+	Tile is a 2D JavaScript tile engine for the HTML5 canvas
+	Copyright (C) 2015 Aaron John Schlosser
+
+	Tile is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	Tile is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+// END LICENSE
+// --------------------------------------------------
+
 module.exports = {
 	"templates": {
 		"src": "src/templates/**/*.html",
@@ -35,4 +62,4 @@ module.exports = {
 		"sources": [],
 		"maps": []
 	}
-}
+};
